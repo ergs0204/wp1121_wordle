b00139711e4a37dd842a58285087def000f76423
guan <guan@MSI.localdomain>
2023-12-12 22:08:26 +0800

    update getwords

--- src/app/api/getWord/route.ts
+++ src/app/api/getWord/route.ts
@@ -1,33 +1,36 @@
 import { NextResponse, type NextRequest } from "next/server";
 
 //import CryptoJS from "crypto-js";
-import { and, eq } from "drizzle-orm";
+import { and, eq, sql } from "drizzle-orm";
 
 import { db } from "@/db";
 import { wordCorpusRelationTable, wordsTable } from "@/db/schema";
 import type {} from "@/lib/types/type";
 
-export async function GET(req) {
+export async function GET(req: NextRequest) {
   try {
     console.log("Getting word");
-    const corpusId = req.nextUrl.searchParams.get("corpusId");
-    console.log(corpusId);
-    if (!corpusId) {
+    const corpusIdStr = req.nextUrl.searchParams.get("corpusId");
+    if (!corpusIdStr) {
       return NextResponse.json(
         { error: "corpusId is required" },
         { status: 400 },
       );
     }
+    const corpusId = corpusIdStr ? +corpusIdStr : 1;
+    console.log(corpusId);
+    // const randomWordId = await db
+    //   .execute(sql`select wordId from ${wordCorpusRelationTable} where ${wordCorpusRelationTable.id} = ${corpusId} ORDER BY RANDOM () LIMIT 1`);
     const randomWordId = await db
       .select({ wordId: wordCorpusRelationTable.wordId })
       .from(wordCorpusRelationTable)
       .where(eq(wordCorpusRelationTable.corpusId, corpusId))
-      .orderBy("random()")
-      .fetchOne();
+      .orderBy(sql`RANDOM()`)
+      .limit(1);
     const randomWord = await db
-      .select("word")
+      .select({ word: wordsTable.word })
       .from(wordsTable)
-      .where({ randomWordId });
+      .where(eq(wordsTable.id, randomWordId[0].wordId));
     return NextResponse.json(randomWord);
   } catch (error) {
     return NextResponse.json(
