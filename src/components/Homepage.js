d41658a5f4549b26351fcae095ee9510c33cf8cb
andy06andy06 <andy06andy06@gmail.com>
2023-12-23 15:51:54 +0800

    fix

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,14 +3,14 @@ import Footer from "./Footer";
 
 export default function Homepage (){
 
-    HttpSession session = request.getSession();
-    session.setAttribute("user", user);
-    User user = (User) session.getAttribute("user");
-    if (user == null) {
-        // user is not logged in, redirect to login page
-        response.sendRedirect(request.getContextPath() + "/login");
-        return;
-    }
+    // HttpSession session = request.getSession();
+    // session.setAttribute("user", user);
+    // User user = (User) session.getAttribute("user");
+    // if (user == null) {
+    //     // user is not logged in, redirect to login page
+    //     response.sendRedirect(request.getContextPath() + "/auth");
+    //     return;
+    // }
 
     return (
         <div className="homepage">
