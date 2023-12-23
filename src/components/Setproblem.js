d4dcea9888b57c4751bbca36ebaeb1e0a2442e0f
andy06andy06 <andy06andy06@gmail.com>
2023-12-23 16:55:58 +0800

    set problem

---
+++ src/components/Setproblem.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import dictionary from "./../data/dictionary";
+
+export default function Setproblem() {
+    const [solution, setSolution] = useState();
+
+    return (
+        <div className="set-problem">
+            2
+        </div>
+    );
+}
