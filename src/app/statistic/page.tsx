4bbfc813976269fe551130a14b47a6047f03fce6
andy06andy06 <andy06andy06@gmail.com>
2023-12-28 10:29:21 +0800

    gamestat

--- src/app/statistic/page.tsx
+++ src/app/statistic/page.tsx
@@ -1,17 +1,24 @@
 "use client"
 import React from "react";
 import { useRouter } from "next/navigation";
+import Gamestat from "@/components/Gamestat";
 
 export default function Statistic (){
     const router = useRouter();
 
     // TODO: get personal statistic from api
+
     
 
     return (
         <div className="statistic">
             <button className="back" onClick={() => router.push("/")}>Home</button>
             <h1>Wordle Statistic</h1>
+            <p>Player: user</p>
+            <div>
+                <h2>Single Player</h2>
+                <Gamestat />
+            </div>
         </div>
     );
 };
---
+++ src/components/Gamestat.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Gamestat (){
+
+
+    return (
+        <div className="gamestat">
+            
+        </div>
+    );
+};
+
