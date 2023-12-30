97eff538ecd0546d381108ba87ddf786b2486b42
andy06andy06 <andy06andy06@gmail.com>
2023-12-30 18:40:05 +0800

    fix

--- src/components/Gamestat.js
+++ src/components/Gamestat.js
@@ -2,14 +2,24 @@ import React from "react";
 // import { getScoreboard } from "../app/api/getScoreboard";
 
 export default function Gamestat ({ gameInfos }){
-    // const gameWord = gameInfos.word;
-    // const gameTurn = gameInfos.guesses;
+    const gameWord = gameInfos.word;
+    const gameTurn = gameInfos.guesses;
     // const startTime = gameInfos.startTime;
     // const endTime = gameInfos.endTime;
     // const gameTime = endTime - startTime;
     e.map((gameInfo) => {
         console.log(gameInfo);
     });
+
+    const gamestat = () => {
+        return (
+                <div className="game">
+                    <div className="word">{gameWord}</div>
+                    <div className="turn">{gameTurn}</div>
+                    <div className="time">time</div>
+                </div>
+        );
+    }
 
     return (
         <div className="gamestat">
