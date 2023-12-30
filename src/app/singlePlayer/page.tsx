5c5020a3271ffe4d34c4f77b318420e7abab4a04
ergs0204 <ergs0204@gmail.com>
2023-12-30 02:49:38 -0800

    finish fetch getword

--- src/app/singlePlayer/page.tsx
+++ src/app/singlePlayer/page.tsx
@@ -15,26 +15,27 @@ function SinglePlayer() {
     useEffect(() => {
 
         // TODO: get random word from api
-        // fetch('/api/getWord?corpusId=1')
-        //     .then(response => response.json())
-        //     .then((data) => {
-        //         setSolution(data.word);
-        //         const solution=data.word
-        //         console.log("solution in fetch", solution);
-        //         setWords(data.allWords);
-        //         const words=data.allWords
-        //         console.log("solution", data.word);
-        //     })
-        //     .catch(error => console.error(error));
+        fetch('/api/getWord?corpusId=1')
+            .then(response => response.json())
+            .then((data) => {
+                console.log(data);
+                const solution=data.word;
+                console.log("solution in fetch", solution);
+                setSolution(solution);
+                const words=data.allWords;
+                setWords(words);
+                console.log("solution", data.word);
+            })
+            .catch(error => console.error(error));
         
-        const dictWords = Object.keys(dictionary).filter(
-            dict => dict.length === 5
-        );
-        setWords(dictWords);
-        const wordSize = dictWords.length;
-        let wordIndex = Math.floor(Math.random() * wordSize);
-        const solution = dictWords[wordIndex];
-        setSolution(solution);
+        // const dictWords = Object.keys(dictionary).filter(
+        //     dict => dict.length === 5
+        // );
+        // setWords(dictWords);
+        // const wordSize = dictWords.length;
+        // let wordIndex = Math.floor(Math.random() * wordSize);
+        // const solution = dictWords[wordIndex];
+        // setSolution(solution);
         setBeginTime(new Date().toLocaleTimeString());
         console.log("solution", solution);
     }, []);
