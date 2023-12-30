d110e07fd818cec5a49a59d4ae43fe7cdf270937
andy06andy06 <andy06andy06@gmail.com>
2023-12-30 14:03:58 +0800

    stat

--- src/app/statistic/page.tsx
+++ src/app/statistic/page.tsx
@@ -5,18 +5,31 @@ import Gamestat from "@/components/Gamestat";
 
 export default function Statistic (){
     const router = useRouter();
+    const [user, setUser] = React.useState("");
+    const [score, setScore] = React.useState(0);
+    const [gameInfos, setGameInfos] = React.useState([]);
 
     // TODO: get personal statistic from api
-    const user = "user";
+    fetch('/api/getScoreboard')
+    .then(response => response.json())
+    .then((data) => {
+        const userId = data.userId
+        const score = data.score
+        const gameInfos = data.games
+        setUser(userId)
+        setScore(score)
+        setGameInfos(gameInfos)
+    })
+    .catch(error => console.error(error));
     
 
     return (
         <div className="statistic">
             <button className="back" onClick={() => router.push("/")}>Home</button>
             <h1>Wordle Statistic</h1>
-            <h2>Player: xxxx</h2>
+            <h2>Player: {user}<br></br><br></br>score: {score}</h2>
             <div>
-                <Gamestat user={user}/>
+                <Gamestat gameInfos={gameInfos}/>
             </div>
         </div>
     );
--- src/components/Gamestat.js
+++ src/components/Gamestat.js
@@ -1,20 +1,9 @@
 import React from "react";
 // import { getScoreboard } from "../app/api/getScoreboard";
 
-export default function Gamestat ({ user }){
-
-    const fetchScoreboard = () => {
-        const scoreboard = getScoreboard();
-        console.log(scoreboard);
-        {scoreboard.map((user, i) => {
-            return (
-                <div key={i} className="user">
-                    <div className="username">{user.username}</div>
-                    <div className="score">{user.score}</div>
-                </div>
-            );
-        })}
-    }
+export default function Gamestat ({ gameInfos }){
+    
+    
 
     return (
         <div className="gamestat">
